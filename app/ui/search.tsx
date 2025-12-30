c7ad4ea403e95830492069b1c5d18774f4bb5cdf
giddingit <coding.dlk@gmail.com>
2025-12-30 11:55:56 -0500

    added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,8 +11,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
-    
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); // Reset to first page on new search
+
     if (term) {
       params.set('query', term)
     } else {
